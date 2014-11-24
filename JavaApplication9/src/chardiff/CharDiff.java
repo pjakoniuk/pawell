7472434997323628a5a5122aa6973e4b3612af96
pjakoniuk <pawel.jakoniuk@wp.pl>
2014-11-24 22:12:18 +0100

    pawel

---
+++ JavaApplication9/src/chardiff/CharDiff.java
@@ -0,0 +1,48 @@
+﻿/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+package chardiff;
+
+/**
+*
+* @author Pawel
+*/
+public class CharDiff {
+
+public int Count(String str1,String str2){
+int n = 0;
+int x = 0;
+if(str1.length()==str2.length())
+{
+    for (n=0; n<str1.length(); n++)
+        {
+        if(str1.length()==str2.length())
+        {
+            if (str1.charAt(n)!=str2.charAt(n))
+            {
+                 x++;
+            }
+        }
+    }
+    System.out.println(x);
+    return x;
+}
+
+else
+{
+System.out.println("Różne długości wyrazów.");
+}
+return x;
+}
+
+
+public static void main(String[] args) {
+// TODO code application logic here
+CharDiff chardiff1 = new CharDiff();
+chardiff1.Count("ABCDFE","ABCDEF");
+
+}
+
+}
